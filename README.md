77726039cd7eb9d0e3c48826e5cb85adc1db18d9
Mathieu Méa <mathieu.mea@gmail.com>
2023-08-01 04:37:55 -0400

    Initial release.

---
+++ README.md
@@ -0,0 +1,4 @@
+ca-montreal-rem-light-rail-parser
+=================================
+
+Parser for Montréal REM Light Rail data
---
+++ src/main/java/org/mtransit/parser/ca_montreal_rem_light_rail/MontrealREMLightRailAgencyTools.java
@@ -0,0 +1,114 @@
+package org.mtransit.parser.ca_montreal_rem_light_rail;
+
+import static org.mtransit.commons.RegexUtils.BEGINNING;
+import static org.mtransit.commons.RegexUtils.group;
+import static org.mtransit.parser.Constants.EMPTY;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.gtfs.data.GStop;
+import org.mtransit.parser.mt.data.MAgency;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+// https://rem.info/ (GTFS received by email)
+public class MontrealREMLightRailAgencyTools extends DefaultAgencyTools {
+
+	public static void main(@NotNull String[] args) {
+		new MontrealREMLightRailAgencyTools().start(args);
+	}
+
+	@Nullable
+	@Override
+	public List<Locale> getSupportedLanguages() {
+		return LANG_FR;
+	}
+
+	@Override
+	public boolean defaultExcludeEnabled() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public String getAgencyName() {
+		return "REM";
+	}
+
+	@NotNull
+	@Override
+	public Integer getAgencyRouteType() {
+		return MAgency.ROUTE_TYPE_LIGHT_RAIL;
+	}
+
+	@Override
+	public boolean defaultRouteIdEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean useRouteShortNameForRouteId() {
+		return true;
+	}
+
+	@Override
+	public @Nullable Long convertRouteIdFromShortNameNotSupported(@NotNull String routeShortName) {
+		switch (routeShortName) {
+		case "A":
+			return 1001L;
+		}
+		return super.convertRouteIdFromShortNameNotSupported(routeShortName);
+	}
+
+	@Override
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean defaultAgencyColorEnabled() {
+		return true;
+	}
+
+	private static final String AGENCY_COLOR = "72A300"; // light green // web site CSS
+
+	@NotNull
+	@Override
+	public String getAgencyColor() {
+		return AGENCY_COLOR;
+	}
+
+	@Override
+	public boolean directionFinderEnabled() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
+		tripHeadsign = CleanUtils.keepToFR(tripHeadsign);
+		tripHeadsign = CleanUtils.cleanBounds(getFirstLanguageNN(), tripHeadsign);
+		tripHeadsign = CleanUtils.cleanStreetTypesFRCA(tripHeadsign);
+		return CleanUtils.cleanLabelFR(tripHeadsign);
+	}
+
+	private static final Pattern STARTS_WITH_STATION_ = Pattern.compile(group(BEGINNING + "station "), Pattern.CASE_INSENSITIVE);
+
+	@NotNull
+	@Override
+	public String cleanStopName(@NotNull String gStopName) {
+		gStopName = STARTS_WITH_STATION_.matcher(gStopName).replaceAll(EMPTY);
+		gStopName = CleanUtils.cleanBounds(getFirstLanguageNN(), gStopName);
+		gStopName = CleanUtils.cleanStreetTypesFRCA(gStopName);
+		return CleanUtils.cleanLabelFR(gStopName);
+	}
+
+	@Override
+	public int getStopId(@NotNull GStop gStop) {
+		return Integer.parseInt(gStop.getStopCode()); // use stop code as stop ID
+	}
+}
